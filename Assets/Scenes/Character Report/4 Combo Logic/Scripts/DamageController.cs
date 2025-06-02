3a8cec84a38613d748bf2f0fcad2cf1e4b0c7f37
SebastianEsco <sebastian.escobarm@upb.edu.co>
2025-06-01 21:47:41 -0500

    Cosas de buildear para web

--- Assets/Scenes/Character Report/4 Combo Logic/Scripts/DamageController.cs
+++ Assets/Scenes/Character Report/4 Combo Logic/Scripts/DamageController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
 using UnityEditor;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class DamageController : MonoBehaviour
